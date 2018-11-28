a0683e73691c53f114ac20b82b12b49a2f17f9c2
mean <Nawapan_Mean@hotmail.com>
2018-11-29 00:26:38 +0700

    test

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+
+
+<template>
+
+<v-app>
+
+<Navbar />
+  <v-content>
+
+  <div id="app">
+  <img alt="Vue logo" src="./assets/1234.png"width="640" height="5%" >
+  <router-view>
+  </router-view>
+
+  </div>
+  </v-content>
+
+
+
+
+
+</v-app>
+
+
+
+</template>
+
+<script>
+import Navbar from '@/components/Navbar'
+
+export default {
+  name: 'app',
+  components:{ Navbar},
+
+}
+</script>
+
+<style >
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+
+}
+</style>
